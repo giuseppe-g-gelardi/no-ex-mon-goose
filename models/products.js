8bf355e042b00ea3b4bc70adab642739ec8c75d8
n-rdy <giuseppegelardi@icloud.com>
2021-11-15 15:28:31 -0600

    add productsjs and schema

---
+++ models/products.js
@@ -0,0 +1,9 @@
+const mongoose = requre('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 255 },
+  description: { type: String, required: true },
+  category: { type: String, required: true, minlength: 5, maxlength: 50 },
+  price: { type: Number, required: true },
+  dateModified: { type: Date, default: Date.now },
+})
