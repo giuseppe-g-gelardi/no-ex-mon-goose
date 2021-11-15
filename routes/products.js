bf77a581d2acd7670de50ddcbf0ae0c5abb23785
n-rdy <giuseppegelardi@icloud.com>
2021-11-15 15:40:03 -0600

    start routes

---
+++ routes/products.js
@@ -0,0 +1,7 @@
+const Product = require('../models/products')
+const express = require('express')
+const router = express.Router()
+
+// all endpoints and route handlers go here
+
+module.exports = router
