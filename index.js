fb40969c5bd05f542ecf368230065c4851cdaf5b
n-rdy <giuseppegelardi@icloud.com>
2021-11-15 18:56:15 -0600

    install cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const connectDB = require('./startup/db')
 const express = require('express')
+const cors = require('cors')
+
 const products = require('./routes/products')
 
 const app = express()
 
 connectDB()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/products', products)
 
