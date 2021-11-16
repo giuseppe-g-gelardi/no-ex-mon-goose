2cce840a4e6e948c745d7a77ff97ba1061f71bd9
n-rdy <giuseppegelardi@icloud.com>
2021-11-15 20:19:58 -0600

    add crud functionality to users

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/products', products)
 
-
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`)
 })
 
-// ! test
